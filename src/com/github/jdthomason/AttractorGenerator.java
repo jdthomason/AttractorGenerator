1a93041dd423bd58203469d9203de4ef825c2930
J.D. Thomason <joseph.thomason@snhu.edu>
2018-10-26 19:41:55 -0700

    Implemented Processing library for drawing.

--- src/com/github/jdthomason/AttractorGenerator.java
+++ src/com/github/jdthomason/AttractorGenerator.java
@@ -1,121 +1,95 @@
 package com.github.jdthomason;
 
-import javafx.application.Application;
-import javafx.geometry.Point3D;
-import javafx.scene.*;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Sphere;
-import javafx.scene.transform.Rotate;
-import javafx.stage.Stage;
+import peasy.PeasyCam;
+import processing.core.*;
 import java.util.ArrayList;
-import java.util.Random;
 import com.sun.javafx.geom.Vec3d;
 
-public class AttractorGenerator extends Application {
+@SuppressWarnings("IntegerDivisionInFloatingPointContext")
+public class AttractorGenerator extends PApplet {
 
-    private static final int WIDTH = 950;
-    private static final int HEIGHT = 800;
+    private static final int WIDTH = 1400;
+    private static final int HEIGHT = 600;
 
-    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
-    private static final double BETA = (8/3);
-    private static final double RHO = 28;
-    private static final double SIGMA = 10;
+    private static final float BETA = (8/3);
+    private static final float RHO = 28;
+    private static final float SIGMA = 10;
 
-    @Override
-    public void start(Stage primaryStage){
-        Group group = makeGroup();
-        Camera camera = new PerspectiveCamera();
-        Scene scene = new Scene(group, WIDTH, HEIGHT);
-
-        scene.setFill(Color.DARKGRAY);
-        scene.setCamera(camera);
-
-        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            switch (event.getCode()) {
-                case W:
-                    camera.translateZProperty().set(camera.getTranslateZ() + 100);
-                    break;
-                case A:
-                    camera.translateXProperty().set(camera.getTranslateX() - 10);
-                    break;
-                case S:
-                    camera.translateZProperty().set(camera.getTranslateZ() - 100);
-                    break;
-                case D:
-                    camera.translateXProperty().set(camera.getTranslateX() + 10);
-                case Q:
-                    //noinspection IntegerDivisionInFloatingPointContext
-                    group.getTransforms().add(new Rotate(10, (WIDTH/2), (HEIGHT/2), 0, new Point3D(0,0.1,0)));
-            }
-        });
-
-        primaryStage.setTitle("This is the title");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+    private static float x = 1;
+    private static float y = 1;
+    private static float z = 1;
 
     public static void main (String[] args){
-        launch(args);
+        PApplet.main("com.github.jdthomason.AttractorGenerator");
     }
 
-    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
-    private static Group makeGroup(){
+    public void settings(){
+        size(WIDTH, HEIGHT, P3D);
+    }
+
+    public void setup(){
+        PeasyCam camera = new PeasyCam(this, 1000);
+        surface.setTitle("This is the title");
+    }
+
+    public void draw(){
+        int count = 0;
+        background(0);
+        // Translate position so we start in the center
+
+        // This is so we can see things
+        scale(5);
+
         ArrayList<Vec3d> points = makePoints();
-        Group newGroup = new Group();
 
         for(Vec3d item : points){
-            Sphere s = new Sphere(1);
-            PhongMaterial m = new PhongMaterial();
-
-            m.setDiffuseColor(generateColor());
-            s.setTranslateX(item.x + (WIDTH/2));
-            s.setTranslateY(item.y + (HEIGHT/2));
-            s.setTranslateZ(item.z);
-            s.setMaterial(m);
-
-            newGroup.getChildren().add(s);
+            // Deal with the color based on count number
+            if (count >= 0 && count < 3000){
+                stroke(255,0,0);
+                count++;
+                point((float)item.x, (float)item.y, (float)item.z);
+            }
+            else if (count >= 3000 && count < 6000){
+                stroke(255);
+                count++;
+                point((float)item.x, (float)item.y, (float)item.z);
+            }
+            else if (count >= 6000 && count < 9000){
+                stroke(0,0,255);
+                count++;
+                point((float)item.x, (float)item.y, (float)item.z);
+            }
+            else if (count == 9000){
+                point((float)item.x, (float)item.y, (float)item.z);
+                noLoop();
+            }
         }
-
-        return newGroup;
     }
 
     private static ArrayList<Vec3d> makePoints(){
 
         // Initial values:
-        double x = 1;
-        double y = 1;
-        double z = 1;
+        float x = 2;
+        float y = 1;
+        float z = 1;
 
         // Use dt to signify time distance.
-        double dt = 0.01;
+        float dt = 0.01f;
 
         // Store the points in an array of vectors:
         ArrayList<Vec3d> pointsToDraw = new ArrayList<>();
 
         // Generate the points to draw here.
-        for(int t = 0; t < 10000; t++){
-            double dx = (SIGMA * (y - x)) * dt;
-            double dy = (x * (RHO - z) - y) * dt;
-            double dz = ((x * y) - (BETA * z)) * dt;
+        for(int t = 0; t < 9000; t++){
+            float dx = (SIGMA * (y - x)) * dt;
+            float dy = (x * (RHO - z) - y) * dt;
+            float dz = ((x * y) - (BETA * z)) * dt;
             x = x + dx;
             y = y + dy;
             z = z + dz;
             pointsToDraw.add(new Vec3d(x, y, z));
-            //System.out.println("T: " + t + ", X: " + x + ", Y: " + y + ", Z: " + z);
         }
 
         return pointsToDraw;
-    }
-
-    private static Color generateColor(){
-        Random rand = new Random();
-
-        float r = rand.nextFloat();
-        float g = rand.nextFloat();
-        float b = rand.nextFloat();
-
-        return Color.color(r, g, b);
     }
 }
